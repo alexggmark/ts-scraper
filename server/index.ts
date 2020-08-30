4fd49f6f2430667eb9bd338cf619d661515c8ad0
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-08-31 00:50:30 +0100

    Set up working TS compilation of server environment and webpack for client

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,10 @@
 import * as http from 'http'
 
-const server = http.createServer()
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('okay')
+})
+
 server.listen(3000, () => {
   console.log('TEST')
 })
