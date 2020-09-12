51761567b2c2299d7a9198699c02e838989e8898
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-09-12 23:02:04 +0100

    Cleaned filtering counter

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { render } from 'react-dom'
 import { scraperTest } from './WebScraper'
 
 const App = () => {
   const [articles, setArticles] = useState([])
+  const [articlesCache, setArticlesCache] = useState([])
+  const [blockedCounter, setBlockedCounter] = useState(0)
+  const filterInput = useRef()
+
   useEffect(() => {
     scraperTest()
       .then((res) => {
-        setArticles(JSON.parse(res))
+        const results = JSON.parse(res)
+        setArticlesCache(results)
+        setArticles(results)
       })
   }, [])
+
+  const filterResults = (string: string) => {
+    const filteredArticles = articlesCache.filter((item) => {
+      return !item.title.toLowerCase().includes(string)
+    })
+    setBlockedCounter(articlesCache.length - filteredArticles.length)
+    setArticles(filteredArticles)
+  }
+
+  const handleInput = () => {
+    const input = filterInput.current.value
+    if (input === '') {
+      setArticles(articlesCache)
+      setBlockedCounter(0)
+      return
+    }
+    filterResults(input)
+  }
 
   return (
     <div>
       <h1>TEFSTING</h1>
+      <p>Blocking: {blockedCounter}</p>
+      <input ref={filterInput} onInput={handleInput} placeholder="Filter" />
       <p>{articles.map((item, index) => {
         return (
-          <div className="block">
+          <div className="block" key={index}>
             <p><a href={`https://www.bbc.co.uk${item.url}`}>{item.title}</a></p>
           </div>
         )
