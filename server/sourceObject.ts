acce8ced71048c85fbf819d162c34b6026cd1e76
Alex Gronmark <alex.gronmark@wemakewebsites.com>
2020-09-27 16:08:32 +0100

    Removing > selectors from sourceObject domContentText

--- server/sourceObject.ts
+++ server/sourceObject.ts
@@ -3,7 +3,7 @@ export const sourceObject = [
     route: '/bbc',
     domHeading: '.gs-c-promo-heading',
     domTitle: '.gs-c-promo-heading__title',
-    domContentText: '.story-body__inner > p',
+    domContentText: '.story-body__inner p',
     scrapeUrl: 'https://www.bbc.co.uk/news',
     rawUrl: 'https://www.bbc.co.uk'
   },
@@ -11,7 +11,7 @@ export const sourceObject = [
     route: '/guardian',
     domHeading: '.js-headline-text',
     domTitle: null,
-    domContentText: '.content__article-body > p',
+    domContentText: '.content__article-body p',
     scrapeUrl: 'https://www.theguardian.com/theguardian/mainsection/uknews',
     rawUrl: 'https://www.theguardian.com'
   }
